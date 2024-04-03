35c8338303527f543fc7e593098bb86714a06ef3
Rahul <rahuldevan2019@gmail.com>
2024-04-03 22:43:13 +0530

    congiguration of prisma and mongodb done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,24 +1,77 @@
 import bcrypt from "bcrypt"
+import prisma from "../lib/prisma.js";
 
 
-const register=async(req,res)=>{
-const {username,email,password}=req.body;
+const register = async (req, res) => {
+    const { username, email, password } = req.body;
 
-// hash the password
-const hashedPassword=await bcrypt.hash(password,10)
-console.log(username,email,hashedPassword);
+    try {
+        // hash the password
+        const hashedPassword = await bcrypt.hash(password, 10)
+        // console.log(username, email, hashedPassword);
 
-return hashedPassword;
+        //create a new user and save to DB
 
-//create a new user and save to DB
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+            }
+        })
+        console.log(newUser);
+
+        res.status(201).json({
+            success: true,
+            data: newUser,
+            message: "user Created Successfully"
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: [error.message, "Error in creating the user"]
+        })
+    }
+
 }
 
-const login=(req,res)=>{
-//db operations
+const login = async(req, res) => {
+    const {username,password}=req.body
+
+    try {
+        // checking the user exists 
+
+        const user=await prisma.user.findUnique({
+            where:{username}
+        })
+
+        if(!user){
+            return res.status(401).json({
+                message:"invalid crendetnials"
+            })
+        }
+        // check if the password is correct
+
+        const isPasswordValid=await bcrypt.compare(password,user.password)
+
+        if(!isPasswordValid){
+            return res.status(401).json({
+                message:"invalid crendetnials"
+            })
+        }
+
+        // Generate cookie token and send to the user
+
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:["error in loggin ",error.message]
+        })
+    }
 }
 
-const logout=(req,res)=>{
-//db operations
+const logout = (req, res) => {
+    //db operations
 }
 
 
