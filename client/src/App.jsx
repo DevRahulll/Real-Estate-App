227571ff064d824d96579bbec4007b7e087092c1
Rahul <rahuldevan2019@gmail.com>
2024-04-09 23:48:46 +0530

    almost completed home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import "./layout.scss"
 import Navbar from './components/navbar/Navbar.jsx'
+import HomePage from './pages/homePage/HomePage.jsx'
 function App() {
 
   return (
  <div className='layout'>
+  <div className='navbar'>
   <Navbar/>
+  </div>
+  <div className='content'>
+    <HomePage/>
+  </div>
  </div>
   )
 }
