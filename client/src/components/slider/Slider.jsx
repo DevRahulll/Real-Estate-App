407a00a5b0584c3e72e6ba2fec2736f048f4068d
Rahul <rahuldevan2019@gmail.com>
2024-04-17 21:40:47 +0530

    some bugs fixed

--- client/src/components/slider/Slider.jsx
+++ client/src/components/slider/Slider.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
 import "./slider.scss"
 
-function Slider({images}) {
+function Slider({ images }) {
   return (
     <div className="slider">
-     <div className="bigImage">
-      <img src={images[0]} alt="" />
-      </div> 
+      
+      <div className="fullSlider">
+
+        <div className="arrow">
+          <img src="/arrow.png" alt="" />
+        </div>
+
+        <div className="imgContainer">
+          <img src={images[0]} alt="" />
+        </div>
+
+        <div className="arrow">
+          <img src="/arrow.png" className='rightArrow' alt="" />
+        </div>
+
+        <div className="close">X</div>
+      </div>
+
+      <div className="bigImage">
+        <img src={images[0]} alt="" />
+      </div>
+
       <div className="smallImage">
-        {images.slice(1).map((image,index)=>(
-          <img src={image} alt="" key={index}/>
+        {images.slice(1).map((image, index) => (
+          <img src={image} alt="" key={index} />
         ))}
+        
       </div>
     </div>
   )
