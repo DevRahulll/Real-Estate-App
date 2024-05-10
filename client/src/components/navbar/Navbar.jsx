bef1b0642cf03250de0719e83bb8fec99740dd1e
Rahul <rahuldevan2019@gmail.com>
2024-05-10 16:32:30 +0530

    some basic fixes in the code

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -26,15 +26,21 @@ function Navbar() {
 
         {user ? (
           <div className="user">
-            <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+            <img
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+              alt=""
+            />
             <span>Rahul Dev</span>
+
             <Link to={"/profile"} className="profile">
               <div className="notification">3</div>
               <span>Profile</span>
             </Link>
           </div>
+
         ) : (
-          <><a href="/">SignIn</a>
+          <>
+            <a href="/">SignIn</a>
             <a href="/" className="register">
               SignUp
             </a>
@@ -42,16 +48,19 @@ function Navbar() {
         )}
 
         <div className="menuIcon">
-          <img src="menu.png" alt="" onClick={() => setOpen((prev) => !prev)} />
-
-          <div className={open ? "menu active" : "menu"}>
-            <a href="/">Home</a>
-            <a href="/">About</a>
-            <a href="/">Contact</a>
-            <a href="/">Agents</a>
-            <a href="/">SignIn</a>
-            <a href="/">SignUp</a>
-          </div>
+          <img
+            src="menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">SignIn</a>
+          <a href="/">SignUp</a>
         </div>
       </div>
     </nav>
--- client/src/pages/homePage/HomePage.jsx
+++ client/src/pages/homePage/HomePage.jsx
@@ -7,23 +7,28 @@ function HomePage() {
     <div className="homePage">
       <div className="textContainer">
         <div className="wrapper">
-        <h1 className='title'>Find Real Estate & Get Your Dream Place</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis minima consequuntur totam! Consectetur pariatur fugit reiciendis earum. Perferendis, repudiandae id?</p>
-         <SearchBar/>
-        <div className="boxes">
-          <div className="box">
-            <h1>16+</h1>
-            <h2>Years of Experience</h2>
+          <h1 className='title'>Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis minima consequuntur totam! Consectetur pariatur fugit reiciendis earum. Perferendis, repudiandae id?
+          </p>
+
+          <SearchBar />
+
+          <div className="boxes">
+
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
           </div>
-          <div className="box">
-            <h1>200</h1>
-            <h2>Award Gained</h2>
-          </div>
-          <div className="box">
-            <h1>2000+</h1>
-            <h2>Property Ready</h2>
-          </div>
-        </div>
         </div>
       </div>
       <div className="imgContainer">
